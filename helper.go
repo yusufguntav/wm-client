339a0f53bc9b4fe8fe32ae51d6c897a4ef1f626e
yusufguntav <ysf.guntav@gmail.com>
2025-08-04 22:59:10 +0300

    Enhance request handling: support multipart file uploads and improve error messages in HTTP requests

--- helper.go
+++ helper.go
@@ -6,7 +6,13 @@ import (
 	"fmt"
 	"io"
 	"log"
+	"mime/multipart"
 	"net/http"
+	"os"
+	"path/filepath"
+	"reflect"
+	"strconv"
+	"strings"
 	"time"
 
 	"github.com/yusufguntav/wm-client/models"
@@ -41,7 +47,7 @@ func (c *Client) doRequest(method, endpoint string, payload interface{}) ([]byte
 		}
 
 		if status < 200 || status >= 300 {
-			return respBody, fmt.Errorf("unexpected status code after token refresh: %d", status)
+			return respBody, fmt.Errorf("unexpected status code after token refresh: %d\nResponse:%s", status, respBody)
 		}
 	}
 
@@ -50,12 +56,31 @@ func (c *Client) doRequest(method, endpoint string, payload interface{}) ([]byte
 
 func (c *Client) executeRequest(method, endpoint string, payload interface{}) ([]byte, int, error) {
 	var body io.Reader
-	if payload != nil {
-		data, err := json.Marshal(payload)
-		if err != nil {
-			return nil, 0, fmt.Errorf("json marshal error: %w", err)
+	contentType := "application/json"
+
+	switch p := payload.(type) {
+	case nil:
+	case *bytes.Buffer:
+		body = p
+	case io.Reader:
+		body = p
+	default:
+		if hasMultipartFile(payload) {
+			var b bytes.Buffer
+			w := multipart.NewWriter(&b)
+			if err := writeMultipart(w, payload); err != nil {
+				return nil, 0, fmt.Errorf("multipart encode error: %w", err)
+			}
+			w.Close()
+			body = &b
+			contentType = w.FormDataContentType()
+		} else {
+			data, err := json.Marshal(payload)
+			if err != nil {
+				return nil, 0, fmt.Errorf("json marshal error: %w", err)
+			}
+			body = bytes.NewBuffer(data)
 		}
-		body = bytes.NewBuffer(data)
 	}
 
 	req, err := http.NewRequest(method, fmt.Sprintf("%s%s", c.BaseURL, endpoint), body)
@@ -63,7 +88,7 @@ func (c *Client) executeRequest(method, endpoint string, payload interface{}) ([
 		return nil, 0, fmt.Errorf("request creation error: %w", err)
 	}
 
-	req.Header.Set("Content-Type", "application/json")
+	req.Header.Set("Content-Type", contentType)
 	if c.Token != "" {
 		req.Header.Set("Authorization", "Bearer "+c.Token)
 	}
@@ -80,7 +105,7 @@ func (c *Client) executeRequest(method, endpoint string, payload interface{}) ([
 	}
 
 	if resp.StatusCode < 200 || resp.StatusCode >= 300 {
-		return respBody, resp.StatusCode, fmt.Errorf("unexpected status code: %d", resp.StatusCode)
+		return respBody, resp.StatusCode, fmt.Errorf("unexpected status code: %d\nResponse:%s", resp.StatusCode, respBody)
 	}
 
 	return respBody, resp.StatusCode, nil
@@ -103,5 +128,87 @@ func (c *Client) RefreshToken() error {
 	}
 
 	c.Token = loginResp.Token
+	return nil
+}
+
+func hasMultipartFile(payload interface{}) bool {
+	val := reflect.ValueOf(payload)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	if val.Kind() != reflect.Struct {
+		return false
+	}
+	for i := 0; i < val.NumField(); i++ {
+		ft := val.Field(i)
+		if ft.Kind() == reflect.String && ft.String() != "" {
+			// local file path
+			tag := val.Type().Field(i).Tag.Get("form")
+			if strings.Contains(tag, "file") {
+				return true
+			}
+		}
+		if fh, ok := ft.Interface().(*multipart.FileHeader); ok && fh != nil {
+			return true
+		}
+	}
+	return false
+}
+
+func writeMultipart(w *multipart.Writer, payload interface{}) error {
+	val := reflect.ValueOf(payload)
+	if val.Kind() == reflect.Ptr {
+		val = val.Elem()
+	}
+	typ := val.Type()
+	for i := 0; i < val.NumField(); i++ {
+		field := val.Field(i)
+		fieldType := typ.Field(i)
+		formTag := fieldType.Tag.Get("form")
+		if formTag == "" {
+			continue
+		}
+
+		switch f := field.Interface().(type) {
+		case string:
+			// Eğer dosya alanı ve path verilmişse dosyayı oku
+			if (strings.Contains(strings.ToLower(formTag), "file") || formTag == "bulk_wp_file") && f != "" {
+				file, err := os.Open(f)
+				if err != nil {
+					return fmt.Errorf("file open error for %s: %w", f, err)
+				}
+				defer file.Close()
+
+				part, err := w.CreateFormFile(formTag, filepath.Base(f))
+				if err != nil {
+					return err
+				}
+				if _, err := io.Copy(part, file); err != nil {
+					return err
+				}
+			} else {
+				w.WriteField(formTag, f)
+			}
+		case bool:
+			w.WriteField(formTag, strconv.FormatBool(f))
+		case int, int64:
+			w.WriteField(formTag, fmt.Sprintf("%v", f))
+		case *multipart.FileHeader:
+			if f != nil {
+				file, err := f.Open()
+				if err != nil {
+					return err
+				}
+				defer file.Close()
+				part, err := w.CreateFormFile(formTag, f.Filename)
+				if err != nil {
+					return err
+				}
+				if _, err := io.Copy(part, file); err != nil {
+					return err
+				}
+			}
+		}
+	}
 	return nil
 }
--- models/wp.go
+++ models/wp.go
@@ -1,7 +1,6 @@
 package models
 
 import (
-	"mime/multipart"
 	"time"
 )
 
@@ -15,19 +14,18 @@ type SendWpRequest struct {
 }
 
 type PreviewWpRequest struct {
-	Numbers            string                `form:"numbers"`               // "+905347882505"
-	Message            string                `form:"message"`               // "dawdawdwa"
-	CampaignName       string                `form:"campaign_name"`         // "testtt"
-	RegID              string                `form:"reg_id"`                // "2817162021"
-	Now                bool                  `form:"now"`                   // true
-	SendSpeed          int                   `form:"send_speed"`            // 4
-	SendDate           string                `form:"send_date"`             // "2025-08-01T13:53:46+03:00"
-	BulkWpExcelNumbers string                `form:"bulk_wp_excel_numbers"` // "[object Object]" (JSON veya raw string gelebilir)
-	BulkWpFile         *multipart.FileHeader `form:"bulk_wp_file"`          // Dosya
-	AddCancelLink      bool                  `form:"add_cancel_link"`       // false
-	TzString           string                `form:"tz_string"`             // "(UTC+03:00) Istanbul"
+	Numbers            string `json:"numbers" form:"numbers"`                             // "+905347882505"
+	Message            string `json:"message" form:"message"`                             // "dawdawdwa"
+	CampaignName       string `json:"campaign_name" form:"campaign_name"`                 // "testtt"
+	RegID              string `json:"reg_id" form:"reg_id"`                               // "2817162021"
+	Now                bool   `json:"now" form:"now"`                                     // true
+	SendSpeed          int    `json:"send_speed" form:"send_speed"`                       // 4
+	SendDate           string `json:"send_date" form:"send_date"`                         // "2025-08-01T13:53:46+03:00"
+	BulkWpExcelNumbers string `json:"bulk_wp_excel_numbers" form:"bulk_wp_excel_numbers"` // "[object Object]"
+	BulkWpFile         string `json:"-" form:"bulk_wp_file"`                              // Dosya (JSON gönderiminde yok)
+	AddCancelLink      bool   `json:"add_cancel_link" form:"add_cancel_link"`             // false
+	TzString           string `json:"tz_string" form:"tz_string"`                         // "(UTC+03:00) Istanbul"
 }
-
 type BulkWpPreviewResponse struct {
 	ID                      string    `json:"id"`
 	TotalSuccessMembers     int       `json:"total_success_members"`
